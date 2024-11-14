bc73670c1aef3020016078c81cd2f2d88a464cb1
abhradeepd <abhradeep.d9@gmail.com>
2024-11-14 16:51:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,78 @@ data = pd.read_csv('data/train.csv')
 data.head(5)
 # %%
 print(f"Number of Observations in data are {data.shape[0]}")
+
+#%%
+# %%
+print(data.info())
+
+missing_values_count = data.isnull().sum()
+
+# Create a bar plot of missing values
+plt.figure(figsize=(10, 6))
+missing_values_count.plot(kind='bar', color='skyblue')
+
+# Add labels and title
+plt.title('Number of Missing Values per Column', fontsize=16)
+plt.xlabel('Columns', fontsize=14)
+plt.ylabel('Number of Missing Values', fontsize=14)
+
+# Show the plot
+plt.show()
+
+#%%
+# Count the number of rows before dropping
+rows_before_drop = len(data)
+
+# Drop rows with missing values
+data = data.dropna()
+
+# Count the number of rows after dropping
+rows_after_drop = len(data)
+
+# Calculate the number of rows dropped
+rows_dropped = rows_before_drop - rows_after_drop
+
+# Display the number of rows dropped
+print("Number of rows dropped:", rows_dropped)
+
+#%%
+# Group by 'is_duplicate' and count the number of observations for each group
+grouped_data = data.groupby("is_duplicate")['id'].count()
+
+total_questions = grouped_data.sum()
+percentages = (grouped_data / total_questions) * 100
+
+colors = ['lightblue', 'lightcoral']  
+plt.figure(figsize=(10, 8))  
+ax = percentages.plot(kind='bar', color=colors, edgecolor='black')
+
+plt.title('Distribution of Duplicate and Non-duplicate Questions', fontsize=16)
+plt.xlabel('is_duplicate', fontsize=14)
+plt.ylabel('Percentage of Questions', fontsize=14)
+
+for p in ax.patches:
+    ax.annotate(f"{p.get_height():.2f}%", (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='center', xytext=(0, 10), textcoords='offset points', fontsize=12)
+
+ax.set_xticklabels(['Non-duplicate', 'Duplicate'], rotation=0)
+
+plt.show()
+
+#%%
+qids = pd.Series(data['qid1'].tolist() + data['qid2'].tolist())
+unique_qs = len(np.unique(qids))
+qs_morethan_onetime = np.sum(qids.value_counts() > 1)
+
+print('Total number of Unique Questions: {}\n'.format(unique_qs))
+print('Number of unique questions that appear more than one time: {} ({}%)\n'.format(qs_morethan_onetime, round(qs_morethan_onetime/unique_qs*100,2)))
+print('Max number of times a single question is repeated: {}\n'.format(max(qids.value_counts())))
+
+#%%
+all_questions = pd.concat([data['question1'], data['question2']], ignore_index=True)
+
+# Display the top 10 most common questions
+top_10_common_questions = all_questions.value_counts().head(10)
+
+print("Top 10 Most Common Questions:")
+print(top_10_common_questions)
